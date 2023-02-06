abde9f968346a9e03820b8854a1ab4ea5931aa0e
Mike <mikej.miller440@gmail.com>
2023-02-06 12:46:38 -0600

    changed host back to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-const sequelize = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
-// const sequelize = process.env.JAWSDB_URL
-// 	? new Sequelize(process.env.JAWSDB_URL)
-// 	: new Sequelize(
-// 		process.env.DB_NAME,
-// 		process.env.DB_USER,
-// 		process.env.DB_PASSWORD,
-// 		{
-// 			host: "localhost",
-// 			dialect: "mysql",
-// 			port: 3306
-// 		}
-// 	);
+// const sequelize = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+const sequelize = process.env.JAWSDB_URL
+	? new Sequelize(process.env.JAWSDB_URL)
+	: new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		{
+			host: "localhost",
+			dialect: "mysql",
+			port: 3306
+		}
+	);
 
 
 module.exports = sequelize;
